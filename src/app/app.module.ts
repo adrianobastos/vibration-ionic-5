fb974bedaf87aebac5bfd7d50b37d76b5775140a
Adriano Bastos <adriano.bastos@araisan.io>
2021-01-15 14:35:18 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { VibrationOriginal } from '@ionic-native/vibration';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    VibrationOriginal,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Vibration, VibrationOriginal } from '@ionic-native/vibration';
+import { from } from 'rxjs';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
-  constructor() {}
+  constructor(
+    public navCtrl: NavController,
+    public vibration: VibrationOriginal
+  ) { }
 
+  vibrate(){
+    this.vibration.vibrate(1000);
+  }
 }
